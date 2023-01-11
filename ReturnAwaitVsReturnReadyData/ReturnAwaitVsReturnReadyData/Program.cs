c006e4257e5b159c83bc2009b2e23d26416a8725
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2023-01-11 09:35:59 +0100

    ReturnAwaitVsReturnReadyData

---
+++ ReturnAwaitVsReturnReadyData/ReturnAwaitVsReturnReadyData/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReturnAwaitVsReturnReadyData;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+TestClass tc = new TestClass();
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
+Console.WriteLine("Before t1: " + sw.Elapsed.ToString());
+Task<string> t1 = tc.ReturnString();
+Console.WriteLine("After t1: " + sw.Elapsed.ToString());
+if (t1.IsCompleted)
+{
+    Console.WriteLine(tc.ReturnString().Result);
+}
+
+Console.WriteLine("Before t2: " + sw.Elapsed.ToString());
+Task<string> t2 = tc.ReturnTaskString();
+Console.WriteLine("After t2: " + sw.Elapsed.ToString());
+if (t2.IsCompleted)
+{
+    Console.WriteLine(tc.ReturnTaskString().Result);
+}
+
+Console.WriteLine("Before s2: " + sw.Elapsed.ToString());
+string s2 = await tc.ReturnTaskString();
+Console.WriteLine("After s2: " + sw.Elapsed.ToString());
+
+
+Console.WriteLine("Before s3: " + sw.Elapsed.ToString());
+string s3 = await tc.ReturnTaskStringWithoutFirstAwait();
+Console.WriteLine("After s3: " + sw.Elapsed.ToString());
+if (t2.IsCompleted)
+{
+    Console.WriteLine(tc.ReturnTaskStringWithoutFirstAwait().Result);
+}
+
+Console.WriteLine("Before s4: " + sw.Elapsed.ToString());
+string s4 = await tc.ReturnTaskStringNoAwaitAtAll();
+Console.WriteLine("After s4: " + sw.Elapsed.ToString());
+if (t2.IsCompleted)
+{
+    Console.WriteLine(tc.ReturnTaskStringNoAwaitAtAll().Result);
+}
+
+Console.WriteLine("DONE");
+
+Console.ReadKey();
---
+++ ReturnAwaitVsReturnReadyData/ReturnAwaitVsReturnReadyData/TestClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnAwaitVsReturnReadyData
+{
+    internal class TestClass
+    {
+
+        internal async Task<string> ReturnString()
+        {
+            await Task.Delay(3000);
+            return "aaaaa";
+        }
+
+        internal async Task<string> ReturnTaskString()
+        {
+            return await Task.Run<string>(async () =>
+            {
+                await Task.Delay(3000);
+                return "bbbbb";
+            });
+        }
+
+        internal Task<string> ReturnTaskStringWithoutFirstAwait()
+        {
+            return Task.Run<string>(async () =>
+            {
+                await Task.Delay(3000);
+                return "bbbbb";
+            });
+        }
+
+        internal Task<string> ReturnTaskStringNoAwaitAtAll()
+        {
+            return Task.Run<string>(async () =>
+            {
+                Task.Delay(3000);
+                return "bbbbb";
+            });
+        }
+    }
+}
