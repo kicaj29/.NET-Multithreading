b23993336fa43a6c54359c2b07f0a01ac046f2a9
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2023-04-05 16:21:32 +0200

    ForeachWithAsync

---
+++ ExceptionHandlingInTasks/ExceptionHandlingInTasks.AspNetCore/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace ExceptionHandlingInTasks.AspNetCore
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine("CAUGHT BY ExceptionHandlerMiddleware :" + exc.Message);
+            }
+        }
+    }
+}
---
+++ ForeachWithAsync/ForeachWithAsync/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading;
+
+Console.WriteLine("Hello, World!");
+
+// 1.
+List<int> list = new List<int> { 1, 2, 3};
+list.ForEach(async (item) =>
+{
+    Console.WriteLine($"ForEach Waiting: {item}");
+    await Task.Delay(2000);
+    Console.WriteLine($"ForEach Completed: {item}");
+});
+Console.WriteLine("After .ForEach"); // List.ForEach will be executed immediately because there is no await!
+
+// 2.
+Parallel.ForEach(list, async (item) =>
+{
+    Console.WriteLine($"Parallel.ForEach Waiting: {item}");
+    await Task.Delay(2000);
+    Console.WriteLine($"Parallel.ForEach Completed: {item}");
+});
+Console.WriteLine("After Parallel.ForEach"); // After Parallel.ForEach will be executed immediately because there is no await!
+
+// 3.
+await Parallel.ForEachAsync(list, async(item, cancellationToken) =>
+{
+    Console.WriteLine($"Parallel.ForEachAsync Waiting: {item}");
+    await Task.Delay(2000);
+    Console.WriteLine($"Parallel.ForEachAsync Completed: {item}");
+});
+Console.WriteLine("After Parallel.ForEachAsync"); // After Parallel.ForEachAsync will be executed when all tasks are done
+
+// 4.
+var tasks = list.Select(async item =>
+{
+    Console.WriteLine($"Select Waiting: {item}");
+    await Task.Delay(2000);
+    Console.WriteLine($"Select Completed: {item}");
+});
+await Task.WhenAll(tasks);
+Console.WriteLine("After .Select"); // After .Select will be executed when all tasks are done.
+
+
+// 4.
+
+
+Console.ReadKey();
+
