b39f31ce1fabfb87bea0e498eb2a6c5f7ff002fa
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2023-08-01 10:22:04 +0200

    UsingTaskYield

---
+++ UsingTaskYield/UsingTaskYield/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UsingTaskYield;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}"); // Print a number
+await Task.CompletedTask;
+Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}"); // Print the same number as above
+await Task.FromResult(1);
+Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}"); // Still the same number as above
+
+// Task.Yield can force an asynchronous method to complete asynchronously.
+// It means that the code after `Yield` will be executed on a different thread.
+await Task.Yield();
+Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}"); // Print a different number
+
+// More here: https://duongnt.com/task-yield/
+
+TestTaskYield test = new TestTaskYield();
+
+Func<Task> asyncHandler = async () => await Task.Delay(500);
+// await test.DriverMethod(asyncHandler);
+//  This will print:
+/*  Executing handler...
+    Executing handler...
+    Executing handler...
+    Executing handler...
+    ================ TASK CANCEL ================
+*/
+
+
+Func<Task> completedTaskHandler = () => Task.CompletedTask;
+//await test.DriverMethod(completedTaskHandler);
+// Program runs into an infinite loop.
+/* Executing handler...
+ * Executing handler...
+ * Executing handler...
+ * Executing handler...
+ * ...
+ */
+
+
+
+Func<Task> taskYieldHandler = async () => await Task.Yield();
+await test.DriverMethod(taskYieldHandler);
+//  This will print:
+/*  ...
+ *  Executing handler...
+ *  Executing handler...
+ *  Executing handler...
+ *  Executing handler...
+ *  Executing handler...
+    ================ TASK CANCEL ================
+*/
+
+
+Console.WriteLine("DONE");
+Console.ReadKey();
---
+++ UsingTaskYield/UsingTaskYield/TestTaskYield.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingTaskYield
+{
+    public class TestTaskYield
+    {
+        private async Task RunUntilCancelAsync(Func<Task> handler, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Console.WriteLine("Executing handler...");
+                // When we reach the command await handler(),
+                // because Task.CompletedTask is already completed,
+                // the runtime keeps executing RunUntilCancelAsync synchronously and doesn’t yield control back
+                // to the current context.
+                await handler();
+            }
+            Console.WriteLine("================ TASK CANCEL ================");
+        }
+
+        public async Task DriverMethod(Func<Task> handler)
+        {
+            var cts = new CancellationTokenSource();
+
+            var runTask = RunUntilCancelAsync(handler, cts.Token);
+            await Task.Delay(2000);
+            cts.Cancel();
+            await runTask;
+        }
+    }
+}
