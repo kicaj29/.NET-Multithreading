76b745f879c7585104f46270981ab0e38c40fc7c
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2025-05-15 22:05:50 +0200

    AsyncEventHandlerExample

---
+++ AsyncEventHandlerExample/AsyncEventHandlerExample/AsyncEventHandlerOwner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncEventHandlerExample
+{
+    public delegate Task AsyncEventHandler<in AsyncEventArgs>(object sender, AsyncEventArgs e);
+
+    public class AsyncEventHandlerOwner
+    {
+        private AsyncEventHandler<AsyncEventArgs>? _receivedAsync;
+
+        public event AsyncEventHandler<AsyncEventArgs> ReceivedAsync
+        {
+            add
+            {
+                _receivedAsync += value;
+            }
+            remove
+            {
+                _receivedAsync -= value;
+            }
+        }
+
+        public async Task OnReceivedAsync(AsyncEventArgs e)
+        {
+            var handlers = _receivedAsync;
+            if (handlers is not null)
+            {
+                var invocationList = handlers.GetInvocationList();
+                var tasks = new Task[invocationList.Length];
+                for (int i = 0; i < invocationList.Length; i++)
+                {
+                    tasks[i] = ((AsyncEventHandler<AsyncEventArgs>)invocationList[i])(this, e);
+                }
+                await Task.WhenAll(tasks);
+            }
+        }
+    }
+}
---
+++ AsyncEventHandlerExample/AsyncEventHandlerExample/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace AsyncEventHandlerExample
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Started");
+
+            AsyncEventHandlerOwner asyncEventHandlerOwner = new();
+
+            for(int i = 0; i < 10; i++)
+            {
+                asyncEventHandlerOwner.ReceivedAsync += async (model, ea) => await ProcessMessageWithAsyncAwait(ea);
+            }
+
+            await asyncEventHandlerOwner.OnReceivedAsync(new AsyncEventArgs());
+
+            Console.WriteLine("Finished");
+        }
+
+        static private async Task ProcessMessageWithAsyncAwait(AsyncEventArgs args)
+        {
+            Console.WriteLine("ProcessMessageWithAsyncAwait started");
+            await Task.Delay(1);
+            Task processingTask = Task.Run(async () =>
+            {
+                await InternalProcessing();
+            });
+
+            await processingTask.ContinueWith(t =>
+            {
+                Console.WriteLine("ProcessMessageWithAsyncAwait ContinueWith");
+            });
+
+            Console.WriteLine("ProcessMessageWithAsyncAwait finished");
+        }
+
+        static private async Task InternalProcessing()
+        {
+            Console.WriteLine("InternalProcessing started");
+            await Task.Delay(5000);
+            Console.WriteLine("InternalProcessing finished");
+        }
+    }
+}
